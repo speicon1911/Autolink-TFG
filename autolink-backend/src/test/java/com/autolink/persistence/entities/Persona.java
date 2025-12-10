fb10cd49a8ca9c18b093fa8cfb98d45c303470f7
speicon1911 <salvadorpeinado111906@gmail.com>
2025-12-10 08:21:53 +0100

    Add test stubs for Persona and TipoVehiculo
    Introduced empty test class Persona and enum TipoVehiculo in the test directory to set up the structure for future test implementations.

---
+++ autolink-backend/src/test/java/com/autolink/persistence/entities/Persona.java
@@ -0,0 +1,5 @@
+package com.autolink.persistence.entities;
+
+public class Persona {
+
+}
---
+++ autolink-backend/src/test/java/com/autolink/persistence/entities/enums/TipoVehiculo.java
@@ -0,0 +1,5 @@
+package com.autolink.persistence.entities.enums;
+
+public enum TipoVehiculo {
+	MOTOCICLETA, TURISMO, CAMION, AUTOBUS, FURGONETA, MOTOCARRO, QUAD 
+}
