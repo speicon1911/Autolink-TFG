7543606441beb79f448dba456bba872120903bcf
speicon1911 <salvadorpeinado111906@gmail.com>
2025-12-11 19:11:12 +0100

    Add layout, routing, and UI for main pages
    Implemented grid layout in app.css and set up Angular routes for home, catalog, login, register, profile, and vehicles. Updated cabecera to use routerLink navigation and imported RouterLink. Added styled HTML and CSS for catalogo, login, perfil, and registro pages, replacing placeholder content with basic UI and forms.

--- autolink-frontend/src/app/app.routes.ts
+++ autolink-frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Catalogo } from './pages/catalogo/catalogo';
+import { Login } from './pages/login/login';
+import { Registro } from './pages/registro/registro';
+import { Perfil } from './pages/perfil/perfil';
+import { Vehiculo } from './pages/vehiculo/vehiculo';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'home', component: Home},
+    {path: 'catalogo', component: Catalogo},
+    {path: 'login', component: Login},
+    {path: 'registro', component: Registro},
+    {path: 'perfil', component: Perfil},
+    {path: 'vehiculos', component: Vehiculo},
+    {path: '', component: Home},
+];
--- autolink-frontend/src/app/core/cabecera/cabecera.ts
+++ autolink-frontend/src/app/core/cabecera/cabecera.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-cabecera',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cabecera.html',
   styleUrl: './cabecera.css',
 })
