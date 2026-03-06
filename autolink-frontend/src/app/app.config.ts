6aa57e8070736f1a33fbb6debbe3ebab3eb0882b
speicon1911 <salvadorpeinado111906@gmail.com>
2026-03-06 15:54:19 +0100

    Frontend
    Se elimina anterior frontend para realizar uno nuevo apoyado por IA

--- autolink-frontend/src/app/app.config.ts
+++ autolink-frontend/src/app/app.config.ts
@@ -1,15 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideHttpClient()
+    provideRouter(routes), provideClientHydration(withEventReplay())
   ]
 };
--- autolink-frontend/src/app/app.routes.ts
+++ autolink-frontend/src/app/app.routes.ts
@@ -1,18 +1,3 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
-import { Catalogo } from './pages/catalogo/catalogo';
-import { Login } from './pages/login/login';
-import { Registro } from './pages/registro/registro';
-import { Perfil } from './pages/perfil/perfil';
-import { Vehiculo } from './pages/vehiculo/vehiculo';
 
-export const routes: Routes = [
-    {path: '', component: Home},
-    {path: 'home', component: Home},
-    {path: 'catalogo', component: Catalogo},
-    {path: 'login', component: Login},
-    {path: 'registro', component: Registro},
-    {path: 'perfil', component: Perfil},
-    {path: 'vehiculos', component: Vehiculo},
-    {path: '', component: Home},
-];
+export const routes: Routes = [];
