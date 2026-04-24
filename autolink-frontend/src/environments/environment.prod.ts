ed3afd117a7abe50e4e21e9832f292d1bbacce1d
speicon1911 <salvadorpeinado111906@gmail.com>
2026-04-24 14:42:09 +0200

    Update environment.prod.ts

--- autolink-frontend/src/environments/environment.prod.ts
+++ autolink-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://tu-dominio-produccion.com' // URL original de backup
-  apiUrl: 'http://192.168.18.42:8080' // Nueva URL del VPS
+  // Usamos window.location.hostname para adaptarnos a cualquier IP/dominio dinámicamente
+  apiUrl: `http://${window.location.hostname}:8080`
 };
